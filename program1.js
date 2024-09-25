b2dd5f2e1f50ca2d01d18fd240ac09cc81373e27
Jone son <131458104+Ashwani00001@users.noreply.github.com>
2024-09-25 15:41:52 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,21 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
-    
-};
+function isValidParentheses(s) {
+    const stack = [];
+    const mapping = { ')': '(', '}': '{', ']': '[' };
 
-module.exports = { isValid };
+    for (let char of s) {
+        if (mapping[char]) { 
+            if (!stack || stack[stack.length - 1] !== mapping[char]) {
+                return false;
+            }
+            stack.pop();
+        } else { 
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+}
 
 
+const s = "()[]{}";
+console.log(isValidParentheses(s)); 
