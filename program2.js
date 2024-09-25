c987d73dd872b58ed88ab44568e532eb0618a21a
Jone son <131458104+Ashwani00001@users.noreply.github.com>
2024-09-25 15:45:07 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,30 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    const romanNumerals = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        const currentChar = s[i];
+        const nextChar = s[i + 1];
+
+        if (romanNumerals[currentChar] < romanNumerals[nextChar]) {
+            result -= romanNumerals[currentChar];
+        } else {
+            result += romanNumerals[currentChar];
+        }
+    }
+
+    return result;
+}
 
 
-module.exports={romanToInt}
+const romanNumeral = "MCMXCIV";
+const integerValue = romanToInt(romanNumeral);
+console.log(integerValue); 
